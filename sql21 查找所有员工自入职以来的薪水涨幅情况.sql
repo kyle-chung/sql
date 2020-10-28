673f80171f0a5feabb0bcce5ae141ddba4b49551
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-10-28 17:51:51 +0800

    Create sql21 查找所有员工自入职以来的薪水涨幅情况.sql

---
+++ sql21 查找所有员工自入职以来的薪水涨幅情况.sql
@@ -0,0 +1,27 @@
+查找所有员工自入职以来的薪水涨幅情况，给出员工编号emp_no以及其对应的薪水涨幅growth，并按照growth进行升序
+（注:可能有employees表和salaries表里存在记录的员工，有对应的员工编号和涨薪记录，但是已经离职了，离职的员工salaries表的最新的to_date!='9999-01-01'，这样的数据不显示在查找结果里面）
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL, --  '入职时间'
+PRIMARY KEY (`emp_no`));
+CREATE TABLE `salaries` (
+`emp_no` int(11) NOT NULL,
+`salary` int(11) NOT NULL,
+`from_date` date NOT NULL, --  '一条薪水记录开始时间'
+`to_date` date NOT NULL, --  '一条薪水记录结束时间'
+PRIMARY KEY (`emp_no`,`from_date`));
+
+两张表，两次join！
+
+select a.emp_no, (b.salary - c.salary) as growth
+from
+employees as a
+    inner join salaries as b
+    on a.emp_no = b.emp_no and b.to_date = '9999-01-01'
+    inner join salaries as c
+    on a.emp_no = c.emp_no and a.hire_date = c.from_date 
+order by growth asc
