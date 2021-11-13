e8dc2d09009c710291f7d1331507c95d5185bdc7
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-11-13 10:42:04 +0800

    Update limit.sql

--- limit.sql
+++ limit.sql
@@ -2,3 +2,7 @@ limit n
 limit 0,n
 limit n offset 0
 all the same!
+
+The LIMIT will reduce the number of rows to return, and the optional OFFSET will specify where to begin counting the number rows from.
+
+
